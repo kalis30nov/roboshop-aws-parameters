6093525f5f4b56dfda4c597bf74f9a69c834ad54
kalis <kalis30nov@yahoo.com>
2023-05-16 11:28:38 +0530

    update

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "aws_ssm_parameter" "parameter" {
   count = lenght(var.parameters)  
   name  = var.parameters[count.index].name
-  type  = var.parameters[count.index].value
+  value  = var.parameters[count.index].value
   type = "String"
 }

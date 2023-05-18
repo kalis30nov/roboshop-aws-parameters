68b211a771f9f7176f8feba4b04e9f18765db5fe
kalis <kalis30nov@yahoo.com>
2023-05-18 06:09:48 +0530

    update

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ resource "aws_ssm_parameter" "parameters" {
   name  = var.parameters[count.index].name
   value  = var.parameters[count.index].value
   type = "String"
-  key_id = a977a2e7-5100-41ff-aa73-9d83f1a4ebee
+  key_id = "a977a2e7-5100-41ff-aa73-9d83f1a4ebee"
 }
 
 resource "aws_ssm_parameter" "passwords" {
@@ -11,5 +11,5 @@ resource "aws_ssm_parameter" "passwords" {
   name  = var.passwords[count.index].name
   value  = var.passwords[count.index].value
   type = "SecureString"
-  key_id = a977a2e7-5100-41ff-aa73-9d83f1a4ebee
+  key_id = "a977a2e7-5100-41ff-aa73-9d83f1a4ebee"
 }
